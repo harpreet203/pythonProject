67619da07daa42c9bf3e4eb3df5feab32ba3c50f
Harpreet Singh <naharharpreet1988@gmail.com>
2024-01-30 19:15:30 +0200

    assignments

---
+++ myprogram1.py
@@ -0,0 +1,4 @@
+area = float(input("Enter house area in square meters:"))
+
+areaFeet = area * 10.764
+print(f"Area of house in square feet:{areaFeet}")
---
+++ myprogram2.py
@@ -0,0 +1,6 @@
+score = float(input("Enter the student's score:"))
+
+if score >= 90:
+    print("you have grade A")
+else:
+    print("below grade A")
